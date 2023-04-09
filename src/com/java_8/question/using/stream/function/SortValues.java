e079d9e2d9cf5f1e81c274172d237ab7b968cc9d
Raushanray <raushankuray1998@gmail.com>
2023-04-09 11:26:41 +0530

    sort all the values present in this given list using Stream functions

---
+++ src/com/java_8/question/using/stream/function/SortValues.java
@@ -0,0 +1,13 @@
+package com.java_8.question.using.stream.function;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Given a list of integers, sort all the values present in it using Stream functions?
+public class SortValues {
+	public static void main(String[] args) {
+		List<Integer> myList = Arrays.asList(10, 15, 800, 49, 25, 98, 98, 32, 15);
+		myList.stream().sorted().forEach(System.out::println);
+
+	}
+}
