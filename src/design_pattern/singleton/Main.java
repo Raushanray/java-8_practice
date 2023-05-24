df57d2f92ff1d819d59ea2bef7419025c432c2c1
Raushanray <raushankuray1998@gmail.com>
2023-05-24 17:09:40 +0530

    Using the Reflection API to break the singleton design pattern.

--- src/design_pattern/singleton/Main.java
+++ src/design_pattern/singleton/Main.java
@@ -1,17 +1,36 @@
 package design_pattern.singleton;
 
+import java.lang.reflect.Constructor;
+
 public class Main {
-	public static void main(String[] args) {
-		// Lazy way of creating singleton object
-		Samosa samosa1 = Samosa.getSamosa();
-		System.out.println(samosa1.hashCode());
-
-		Samosa samosa2 = Samosa.getSamosa();
-		System.out.println(samosa2.hashCode());
-
-		// Eager way of creating singleton object
-		System.out.println(Jalebi.getJalebi().hashCode());
-		System.out.println(Jalebi.getJalebi().hashCode());
+	public static void main(String[] args) throws Exception{
+//		// Lazy way of creating singleton object
+//		Samosa samosa1 = Samosa.getSamosa();
+//		System.out.println(samosa1.hashCode());
+//
+//		Samosa samosa2 = Samosa.getSamosa();
+//		System.out.println(samosa2.hashCode());
+//
+//		// Eager way of creating singleton object
+//		System.out.println(Jalebi.getJalebi().hashCode());
+//		System.out.println(Jalebi.getJalebi().hashCode());
+		
+		
+		/*
+		 * Way for  break the singleton pattern
+		 * 1. Reflection API to break the singleton pattern.
+		 * 
+		 * 
+		 */
+		
+		Samosa s1 =  Samosa.getSamosa();
+		System.out.println(s1.hashCode());
+		
+		Constructor<Samosa> constructor = Samosa.class.getDeclaredConstructor();
+		constructor.setAccessible(true);
+		Samosa s2 = constructor.newInstance();
+		System.out.println(s2.hashCode());
+		
 	}
 
 }
